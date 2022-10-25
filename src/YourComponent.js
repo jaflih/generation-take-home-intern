fdb84e53f2e1970cdefebca063e8f19ea2111a6d
jihaneh <jijih@hotmail.fr>
2022-10-25 09:33:30 +0200

    Add favorites markers

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './component/Marker';
 import data from './store_directory.json';
@@ -12,18 +12,44 @@ export default function YourComponent() {
     zoom: 11,
   };
 
+  const [favorites, setfavorites] = useState([]);
+
+  const clickHandler = (i) => {
+    setfavorites([...favorites, i]);
+  };
+
+  const toggleHandler = (i) => {
+    console.log(i);
+    //favorites = favorites.splice(i, 1);
+    const f = favorites;
+    f.pop();
+    setfavorites(f);
+  };
+
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: '100vh', width: '100%', position: 'relative' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A' }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        {data.map((record) => (
-          <Marker lat={record.Coordinates.lat} lng={record.Coordinates.lng} name={record.Name} address={record.Address} />
+        {data.map((record, index) => (
+          <Marker
+            index={index}
+            lat={record.Coordinates.lat}
+            lng={record.Coordinates.lng}
+            name={record.Name}
+            address={record.Address}
+            clickFunction={clickHandler}
+          />
         ))}
       </GoogleMapReact>
+      <div className="my-favorites">
+        {favorites.map((record, i) => (
+          <div onClick={() => toggleHandler(i)}> {record} </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/component/Marker.js
+++ src/component/Marker.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Marker = ({ name, address }) => {
+const Marker = ({ name, address, index, clickFunction }) => {
   return (
-    <div>
+    <div onClick={() => clickFunction(name)}>
       <div className="marker">i</div>
       <div className="marker-info">
         <span>
           <strong>{name}</strong>
         </span>
         <span>{address}</span>
+        <span>Add to favorite</span>
       </div>
     </div>
   );
